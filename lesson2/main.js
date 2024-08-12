cdac725625d5ab849b93681724c2fc95fbbb7d96
Nadykryv <nkryvonis@gmail.com>
2024-08-13 01:40:10 +0200

    add hw

---
+++ lesson2/main.js
@@ -0,0 +1,118 @@
+
+//--------1-------//
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array [0]);
+console.log(array [1]);
+console.log(array [2]);
+console.log(array [3]);
+console.log(array [4]);
+console.log(array [5]);
+console.log(array [6]);
+console.log(array [7]);
+console.log(array [8]);
+console.log(array [9]);
+
+//-------2----------//
+
+let book1 ={
+    title : "One, no one and a hundred thousand",
+    pageCount : 256,
+    genre: 'novel'
+}
+console.log(book1 );
+
+let book2 ={
+    title : "The Betrothed",
+    pageCount : 743,
+    genre: 'historical novel'
+}
+console.log(book2 );
+
+let book3 ={
+    title : "French kiss",
+    pageCount : 480,
+    genre: 'Masters of Action-Packed Novel, thriller'
+}
+console.log(book3 );
+
+//--------3-----------//
+
+let book4 ={
+    title : "One, no one and a hundred thousand",
+    pageCount : 256,
+    genre: 'novel',
+    authors: [
+        {name : 'Luigi Pirandello', age: '1926'}
+    ]
+}
+console.log(book4 );
+
+let book5 ={
+    title : "The Betrothed",
+    pageCount : 743,
+    genre: 'historical novel',
+    authors: [
+        {name : 'Alessandro Manzoni', age: '1840'}
+    ]
+}
+console.log(book5 );
+
+let book6 ={
+    title : "French kiss",
+    pageCount : 480,
+    genre: 'Masters of Action-Packed Novel, thriller',
+    authors: [
+        {name : 'Eric Van Lastbeider', age: '1989'}
+    ]
+}
+console.log(book6 );
+
+//---------4------------//
+
+let users = [
+    {name : "John", username : "Johnson", password: "1hd9033gg",},
+    {name : "Tony", username : "Ant", password: "v89fdc6",},
+    {name : "Katy", username : "Kat", password: "Fh11vgn32",},
+    {name : "Mayk", username : "Myu", password: "10p76ue6",},
+    {name : "Eva", username : "Apple", password: "12hs99mGj",},
+    {name : "Dorothy", username : "Dora", password: "K771xf56",},
+    {name : "Samuel", username : "Sam", password: "kso345Ol",},
+    {name : "Juliana", username : "July", password: "lsi34996",},
+    {name : "Aster", username : "Stella", password: "Ghj6793hg",},
+    {name : "Margherita", username : "Megan", password: "Hdu753b7",},
+]
+
+console.log(users [0].password );
+console.log(users [1].password );
+console.log(users [2].password );
+console.log(users [3].password );
+console.log(users [4].password );
+console.log(users [5].password );
+console.log(users [6].password );
+console.log(users [7].password );
+console.log(users [8].password );
+console.log(users [9].password );
+
+// А почему просто не использовать: console.log(users); ? так сразу выводит в консоль все данные и всего одна строка))))
+
+
+//-------------5------------//
+
+let x = 7;
+if (x) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+//-----------6-----------//
+
+
+
+
+//
+
+
+
+
