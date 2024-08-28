94ea525e80fe77227fb8ba0df0b24b9b67f9794f
Nadykryv <nkryvonis@gmail.com>
2024-08-28 12:43:08 +0200

    add hw mod

--- lesson9/main.js
+++ lesson9/main.js
@@ -21,5 +21,29 @@ console.log(coursesAndDurationArray);
 
 //-------------------------------------------//
 
+let coursesAndDurationArray1 = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+coursesAndDurationArray1.forEach(value => {
+    const div = document.createElement('div');
+    div.classList.add('item')
+    const h1 = document.createElement('h1');
+    h1.classList.add('heading')
+    const p = document.createElement('p');
+    p.classList.add('description')
+    p.style.fontSize = '32px'
+    h1.innerText = value.title
+    p.innerText = value.monthDuration
+    div.append(h1, p)
+    document.body.appendChild(div)
+})
+console.log(coursesAndDurationArray1);
+//----------------------------------------------//
+
 
 
