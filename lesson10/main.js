0bf30de6fe7f6380fbf70235b9a237ec7fb5edb6
Nadykryv <nkryvonis@gmail.com>
2024-08-30 15:53:39 +0200

    add  hw

--- lesson10/main.js
+++ lesson10/main.js
@@ -9,18 +9,70 @@ form1.addEventListener('submit', function (eventObject) {
     let name = this.name.value;
     let surname = this.surname.value;
     let age = this.age.value;
+    let user = {name, surname, age};
+    localStorage.setItem('user', JSON.stringify(user));
     console.log({ name, surname, age });
 
+    let users = JSON.parse(localStorage.getItem('user')) || [];
+    users.push(user);
+    // let stringify = JSON.stringify(users);
+    localStorage.setItem('user', JSON.stringify(users));
+
 });
-let button = form1 [3];
-console.log(button);
-
-button.onclick = function () {
-    document.getElementsByClassName('popup')[0].style.display = 'block';
-
-};
+// let button = form1 [3];
+// console.log(button);
+//
+// button.onclick = function () {
+//     document.getElementsByClassName('popup')[0].style.display = 'block';
+//
+// };
+// console.log(localStorage);
+// localStorage.setItem('asd', 'que');
+// console.log(localStorage.getItem('asd'));
 
 //------------------------------------------//
 
-console.log(localStorage);
+let currentNumber = +localStorage.getItem('number');
+currentNumber += 1;
+localStorage.setItem('number', currentNumber);
+
+document.getElementById('target').innerText = currentNumber;
+
+//-----------------------------------------//
+
+let sessionList;
+if (localStorage.getItem('sessionList')) {
+    sessionList = JSON.parse(localStorage.getItem('sessionList'));
+}else {
+    sessionList = [];
+}
+sessionList.push(new Date());
+localStorage.setItem('sessionList', JSON.stringify(sessionList));
+
+//------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
