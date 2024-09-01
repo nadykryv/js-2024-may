d999e3e72b0eca7a1d4e5809a3181fb7cf9ef57b
Nadykryv <nkryvonis@gmail.com>
2024-09-02 00:05:00 +0200

    add second hw

--- lesson11/main.js
+++ lesson11/main.js
@@ -1,4 +1,4 @@
-
+//-----------------------1-------------------//
 
 const cartsDiv = document.getElementById('carts');
 fetch('https://dummyjson.com/carts')
@@ -25,6 +25,56 @@ fetch('https://dummyjson.com/carts')
 
         }
 
+
+
+    });
+
+//-----------------------2--------------------------//
+
+
+const url = new URL('https://dummyjson.com/recipes');
+url.searchParams.set('limit', '50');
+
+const target = document.getElementsByClassName('target')[0];
+fetch(url)
+    .then(value => value.json())
+    .then(recipesObject => {
+        const {recipes} = recipesObject;
+        for (const recipe of recipes) {
+            console.log(recipe);
+
+            const recipeDiv = document.createElement('div');
+            for (const recipeKey in recipe) {
+                if (Array.isArray(recipe[recipeKey])) {
+                    const arrayAndTitleDiv = document.createElement('div');
+                    const h3 = document.createElement('div');
+                    h3.innerText = `${recipeKey}:`;
+                    const ol = document.createElement('ol');
+                    const array = recipe[recipeKey];
+                    for (const item of array) {
+                        const li = document.createElement('li');
+                        li.innerText = item;
+                        ol.appendChild(li);
+                    }
+                    arrayAndTitleDiv.append(h3, ol);
+                    recipeDiv.appendChild(arrayAndTitleDiv);
+                } else {
+                    if (recipeKey !== 'image') {
+                const keyDiv = document.createElement('div');
+                keyDiv.innerText = `${recipeKey} : ${recipe[recipeKey]}`
+                recipeDiv.appendChild(keyDiv);
+                    }
+                }
+
+            }
+
+            const img= document.createElement('img');
+            img.src = recipe.image;
+            recipeDiv.appendChild(img);
+
+
+            target.appendChild(recipeDiv);
+        }
 
 
     });
