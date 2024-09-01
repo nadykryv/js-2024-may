c492b0163953b058636485c9f5f04d1ca16b0d17
Nadykryv <nkryvonis@gmail.com>
2024-09-01 12:09:22 +0200

    add new hw

---
+++ lesson11/main.js
@@ -0,0 +1,31 @@
+
+
+const cartsDiv = document.getElementById('carts');
+fetch('https://dummyjson.com/carts')
+    .then(value => value.json())
+    .then(cartsObject => {
+        const {carts} = cartsObject;
+        console.log(carts);
+
+        for (const cart of carts) {
+            const div = document.createElement('div');
+            div.classList.add('cartDiv');
+            div.innerText =`
+                "total": ${cart.total},
+               "discountedTotal": ${cart.discountedTotal},
+               "userId": ${cart.userId},
+               "totalProducts": ${cart.totalProducts},
+               "totalQuantity": ${cart.totalQuantity},
+                `;
+
+
+            cartsDiv.appendChild(div);
+
+
+
+        }
+
+
+
+    });
+
